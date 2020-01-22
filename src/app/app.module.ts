e125c0944e434b8abe9f1f425eb7c9dffbb77d2b
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 10:48:24 +0000

    Refactoring: created auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ActivityModule } from './activities/activity.module';
+import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { FormsModule } from '@angular/forms';
-import { ActivityModule } from './activities/activity.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    LoginComponent,
-    RegisterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -27,10 +24,11 @@ import { ActivityModule } from './activities/activity.module';
 
     // only icons I have explicitly imported will end up in the bundle and the remaining ones will be tree-shaken away.
     FontAwesomeModule,
-    
-    AppRoutingModule,
-    
-    ActivityModule
+        
+    ActivityModule,
+    AuthModule,
+
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
