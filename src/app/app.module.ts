8fc3988cfa0882c73a8193d73fca99083d9c5afa
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 09:23:00 +0000

    Created activity-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activities/activity-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityListComponent
   ],
   imports: [
     BrowserModule,
