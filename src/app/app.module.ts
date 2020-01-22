4f43da1f0f40abec8fcbf72a5be60791f7ebca17
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 10:16:30 +0000

    Added ng-bootstrap to the mix!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
@@ -25,6 +25,8 @@ import { ActivityEditComponent } from './activities/activity-edit/activity-edit.
   ],
   imports: [
     BrowserModule,
+
+    NgbModule,
 
     // only icons I have explicitly imported will end up in the bundle and the remaining ones will be tree-shaken away.
     FontAwesomeModule,
