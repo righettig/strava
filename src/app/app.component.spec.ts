ef6f68569d703fb1544f7a9614d633a6fab9e3e4
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 09:08:28 +0000

    Renamed project to 'strava'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'strava2'`, () => {
+  it(`should have as title 'strava'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('strava2');
+    expect(app.title).toEqual('strava');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('strava2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('strava app is running!');
   });
 });
