16b9ee9313eedcab5d9af37b17a9bc7cfdf3af16
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 15:52:29 +0000

    Basic UI for the analytics component; you can now navigate to the full stats page

--- src/app/analytics/analytics.module.ts
+++ src/app/analytics/analytics.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnalyticsComponent } from './analytics.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: "analytics", component: AnalyticsComponent },
+];
 
 @NgModule({
   declarations: [AnalyticsComponent],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
