6a4b8b7ddbeb35e16ae088e64e13bba748079843
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 16:03:21 +0000

    Implemented canActivate guard logic

--- src/app/activities/activity-edit.guard.ts
+++ src/app/activities/activity-edit.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanDeactivate } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanDeactivate, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ActivityEditComponent } from './activity-edit/activity-edit.component';
 
@@ -7,20 +7,28 @@ import { ActivityEditComponent } from './activity-edit/activity-edit.component';
   providedIn: 'root'
 })
 export class ActivityEditGuard implements CanActivate, CanDeactivate<ActivityEditComponent> {
-  
+
+  constructor(private router: Router) { }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let id = +next.url[1].path;
+    if (isNaN(id) || id < 1) {
+      alert('Invalid activity Id');
+      this.router.navigate(['/activities']);
+      return false;
+    }
     return true;
   }
-  
+
   canDeactivate(
     component: ActivityEditComponent,
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot,
     nextState: RouterStateSnapshot
-    ): Observable<boolean|UrlTree>|Promise<boolean|UrlTree>|boolean|UrlTree {
-      return true;
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
   }
 
 }
