f24eea94d061797b0cb943901b1e3c45ead7ed18
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-22 09:41:14 +0000

    Refactoring: created subfolder for activity-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActivityListComponent } from './activities/activity-list.component';
+import { ActivityListComponent } from './activities/activity-list/activity-list.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
