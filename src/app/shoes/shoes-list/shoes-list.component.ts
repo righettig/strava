7bdc1d5275d84890c59ef4124ec47306ec0336c9
Giacomo Righetti <kirkrulez83@gmail.com>
2020-01-24 10:42:44 +0000

    Added reactive forms binding and validation

--- src/app/shoes/shoes-list/shoes-list.component.ts
+++ src/app/shoes/shoes-list/shoes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shoes-list',
@@ -14,8 +15,51 @@ export class ShoesListComponent implements OnInit {
 
   ngOnInit() {
     this.shoesForm = this.fb.group({
+      brand:    ["", Validators.required],
+      model:    ["", Validators.required],
+      nickname:  "",
+      notes:     "",
     });
+
+    const brandControl = this.shoesForm.get("brand");
+    brandControl.valueChanges.subscribe(value => {
+      this.validateBrand(brandControl);
+    })
+
+    const modelControl = this.shoesForm.get("model");
+    modelControl.valueChanges.subscribe(value => {
+      debugger;
+      this.validateModel(modelControl);
+    })
+
+    // this.shoesForm.valueChanges.pipe(
+    //   filter(el => el.)
+    // )
+    //   subscribe(value => {
+    //   debugger;
+    // })
   }
+
+  save() {
+
+  }
+
+  validateBrand(c: AbstractControl) {
+    this.brandValidationMsg = "";
+    if ((c.touched || c.dirty) && !c.valid) {
+      this.brandValidationMsg = "Please enter a brand";
+    }
+  }
+
+  validateModel(c: AbstractControl) {
+    this.modelValidationMsg = "";
+    if ((c.touched || c.dirty) && !c.valid) {
+      this.modelValidationMsg = "Please enter a model";
+    }
+  }
+
+  brandValidationMsg: string; 
+  modelValidationMsg: string; 
 
   shoesForm: FormGroup;
 
