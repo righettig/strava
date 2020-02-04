f7e61a7c84f326737ab1f0dbd3efb0f69115281c
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-04 09:26:42 +0000

    Fixed issue with AuthInterceptor test setup

--- src/app/core/auth-interceptor.service.spec.ts
+++ src/app/core/auth-interceptor.service.spec.ts
@@ -3,7 +3,11 @@ import { TestBed } from '@angular/core/testing';
 import { AuthInterceptor } from './auth-interceptor.service';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [{
+      provide: AuthInterceptor, useClass: AuthInterceptor
+    }]
+  }));
 
   it('should be created', () => {
     const service: AuthInterceptor = TestBed.get(AuthInterceptor);
