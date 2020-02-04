a5a29ba667ce4d61531d0b4fa5e1529b1587a51f
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-04 09:56:41 +0000

    Fixed issue with test setup: races-list; defined re-usable class for testing the activated-route object

--- src/app/features/races/races-list/races-list.component.spec.ts
+++ src/app/features/races/races-list/races-list.component.spec.ts
@@ -3,17 +3,28 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RacesListComponent } from './races-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute } from '@angular/router';
+import { ActivatedRouteStub } from 'src/app/testing/activated-route-stub';
 
 describe('RacesListComponent', () => {
   let component: RacesListComponent;
   let fixture: ComponentFixture<RacesListComponent>;
 
-  // TODO: snapshot.data['resolvedData']
-
   beforeEach(async(() => {
+    let activatedRouteStub = new ActivatedRouteStub();
+    activatedRouteStub.setSnapshotData({
+      resolvedData: {
+        races: [],
+        error: null
+      }
+    })
+
     TestBed.configureTestingModule({
       imports: [SharedModule, RouterTestingModule],
-      declarations: [ RacesListComponent ]
+      declarations: [ RacesListComponent ],
+      providers: [{
+        provide: ActivatedRoute, useValue: activatedRouteStub
+      }]
     })
     .compileComponents();
   }));
