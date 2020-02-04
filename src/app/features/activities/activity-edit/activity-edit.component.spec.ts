683659b920866e05a9c49c0d6b3c1de769af7951
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-04 16:10:13 +0000

    More testing

--- src/app/features/activities/activity-edit/activity-edit.component.spec.ts
+++ src/app/features/activities/activity-edit/activity-edit.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ActivityEditComponent } from './activity-edit.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,63 +6,130 @@ import { of } from 'rxjs';
 import { ActivitiesApiService } from '../services/activities-api.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 describe('ActivityEditComponent', () => {
+
   let component: ActivityEditComponent;
   let fixture: ComponentFixture<ActivityEditComponent>;
   let activitiesApiMock;
 
-  beforeEach(async(() => {
-    activitiesApiMock = jasmine.createSpyObj(['getActivity']);
-    activitiesApiMock.getActivity.and.returnValue(
-      of({
-        "id": 1,
-        "name": "Longrun #1",
-        "description": "20M longrun",
-        "username": "giacomo",
-        "creationDate": "2020-01-28",
-        "category": "run",
-        "subcategory": "longrun",
-        "location": {
-          "lat": 44.3653,
-          "lng": 10.0922
-        },
-        "distance": 20,
-        "kudos": 2
+  describe("when the user is allowed to edit the activity", () => {
+    let router: Router;
+
+    beforeEach(async(() => {
+      activitiesApiMock = jasmine.createSpyObj(['getActivity', 'editActivity']);
+      activitiesApiMock.getActivity.and.returnValue(
+        of({
+          "id": 1,
+          "name": "Longrun #1",
+          "description": "20M longrun",
+          "username": "giacomo",
+          "creationDate": "2020-01-28",
+          "category": "run",
+          "subcategory": "longrun",
+          "location": {
+            "lat": 44.3653,
+            "lng": 10.0922
+          },
+          "distance": 20,
+          "kudos": 2
+        })
+      )
+
+      TestBed.configureTestingModule({
+        imports: [
+          SharedModule,
+          HttpClientModule,
+          RouterTestingModule
+        ],
+        declarations: [ActivityEditComponent],
+        providers: [
+          { provide: AuthService, useValue: { currentUsername: 'giacomo' } },
+          { provide: ActivitiesApiService, useValue: activitiesApiMock }
+        ]
       })
-    )
-  
-    TestBed.configureTestingModule({
-      imports: [
-        SharedModule,
-        HttpClientModule,
-        RouterTestingModule
-      ],
-      declarations: [ActivityEditComponent],
-      providers: [
-        { provide: AuthService, useValue: { currentUsername: 'giacomo' } },
-        { provide: ActivitiesApiService, useValue: activitiesApiMock }
-      ]
-    })
-      .compileComponents();
-  }));
+        .compileComponents();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ActivityEditComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      router = TestBed.get(Router);
+      spyOn(router, 'navigate').and.stub();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ActivityEditComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-  // trying to edit an actity that does not belong to the current user should redirect to 'activities'
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
 
-  // save
+    fit('save should invoke "editActivity" and redirect to the activities page', () => {
+      activitiesApiMock.editActivity.and.returnValue(
+        of({}) // we do not care about the returned value
+      )
 
-  // reset
+      component.save();
 
-  // dirty
+      expect(router.navigate).toHaveBeenCalledWith(['activities']);
+    });
+
+    // reset
+
+    // dirty
+  })
+
+  describe("when the user is NOT allowed to edit the activity", () => {
+    let router: Router;
+
+    beforeEach(async(() => {
+      activitiesApiMock = jasmine.createSpyObj(['getActivity']);
+      activitiesApiMock.getActivity.and.returnValue(
+        of({
+          "id": 1,
+          "name": "Longrun #1",
+          "description": "20M longrun",
+          "username": "FOO", // the user won't be able to edit this activity
+          "creationDate": "2020-01-28",
+          "category": "run",
+          "subcategory": "longrun",
+          "location": {
+            "lat": 44.3653,
+            "lng": 10.0922
+          },
+          "distance": 20,
+          "kudos": 2
+        })
+      )
+
+      TestBed.configureTestingModule({
+        imports: [
+          SharedModule,
+          HttpClientModule,
+          RouterTestingModule
+        ],
+        declarations: [ActivityEditComponent],
+        providers: [
+          { provide: AuthService, useValue: { currentUsername: 'giacomo' } },
+          { provide: ActivitiesApiService, useValue: activitiesApiMock }
+        ],
+      })
+        .compileComponents();
+
+      router = TestBed.get(Router);
+      spyOn(router, 'navigate').and.stub();
+    }));
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(ActivityEditComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should redirect to /activities', () => {
+      expect(router.navigate).toHaveBeenCalledWith(['activities']);
+    });
+  })
 
 });
